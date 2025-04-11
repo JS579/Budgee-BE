92194506e7cc3a3e1ff0345ba0ee67db60f3e850
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-04-11 16:02:11 +0100

    setting up the development and test data is done

---
+++ db/data/development_data/users.js
@@ -0,0 +1,32 @@
+module.exports=[
+    {
+        username:"lisa Oliver",
+        email :"lisaOliver@gmail.com",
+        avatar:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fphotos%2Feducation-woman%2F2&psig=AOvVaw0NJbTczq5toOYvczoYriHT&ust=1744461277180000&source=images&cd=vfe&opi=89978449&ved=0CAsQtaYDahcKEwjo9rHr_s-MAxUAAAAAHQAAAAAQBw",
+        preferences:["currency"= "USD", "privacy_mode"= true,"login_method"="email"]
+    },
+    {
+        username:"Noah James",
+        email :"noahjames@gmail.com",
+        avatar:"https://www.google.com/imgres?q=pictures%20of%20english%20person%20with%20small%20mb&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-photo%2Fportrait-man-sitting-outdoor-table-600nw-2508367691.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fbritish-man-smiling&docid=ew6T0_s2xbDXWM&tbnid=TR-YkJMKbfAqdM&vet=12ahUKEwjd6tOh_8-MAxURWUEAHZCZNEoQM3oFCIABEAA..i&w=400&h=600&hcb=2&ved=2ahUKEwjd6tOh_8-MAxURWUEAHZCZNEoQM3oFCIABEAA",
+        preferences:[ "theme"= "dark", "notifications"= true, "language"= "en", ]
+    },
+    {
+        username:"Luna Henry",
+        email :"lunahenry1@gmail.com",
+        avatar:"https://www.shutterstock.com/shutterstock/photos/1941541432/display_1500/stock-photo-young-serious-indian-professional-business-man-focused-ethnic-male-student-wearing-glasses-working-1941541432.jpg",
+        preferences:["notifications"= true,"two_factor_auth"= true, ]
+    },
+    {
+        username:"Liam Lukas",
+        email :"liamOlukas@gmail.com",
+        avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpNukHluG0ou2x2hi_wJMfz9P6XGQFb4dB4vZx0jbMhKwj9QcpX0z2g7lZ4lZP1YeIdu8&usqp=CAU",
+        preferences:[]
+    },
+    {
+        username:"Amelia Arlo",
+        email :"ameliaarlo@gmail.com",
+        avatar:"https://theartsdesk.com/sites/default/files/styles/amp_metadata_content_image_min_696px_wide/public/mastimages/f2d330db2c1d302715223f7cd15cd5fa.jpg?itok=9mLlEfzk",
+        preferences:["currency"= "USD","theme"= "dark"]
+    }
+    ]
---
+++ db/data/test_data/categories.js
@@ -0,0 +1,33 @@
+module.exports=[
+    {
+    name :Housing,
+    color_id : null,
+    description: "This includes everything from rent or mortgage payments to property taxes, HOA dues, and home maintenance costs.",
+    total_price : 900
+    },
+    {
+    name :food,
+    color_id : null,
+    description: "A collection of items related to cooking, recipes, ingredients.",
+    total_price : 400
+    },
+    {
+    name :Transportation,
+    color_id : null,
+    description: "A different ways of transportation include gas, public transportation costs (including Uber or Lyft rides), oil changes or other routine car",
+     total_price : 300
+    },
+    {
+    name :Utilities,
+    color_id : null,
+    description: "The services that keep your home up and running — things like gas, electricity, water,sewage bills, internet and cell phone bills.",
+    total_price : 200
+    },
+    {
+    name :Medical & Healthcare,
+    color_id : null,
+    description: "Essential medical care such as yearly physicals and dental appointments.",
+    total_price : 200
+    },
+       
+]
