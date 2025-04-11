5d576da92d4d4e298a18f87076e0bfea844c1de7
John Scholefield <johnscho579@gmail.com>
2025-04-11 12:32:39 +0100

    update listener file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
-const path = require("path");
-const fastifyDB = require("@fastify/mongodb");
+// const path = require("path");
+// const fastifyDB = require("@fastify/mongodb");
 const fastifyPlugin = require("fastify-plugin")
 
 const ENV = process.env.NODE_ENV || "development";
@@ -9,14 +9,11 @@ dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
 const uri = process.env.MONGODB_URI;
 
-async function connectDatabase(app) {
-  try {
-    console.log("Registering MongoDB plugin...");
-    await app.register(fastifyDB, { url: uri });
-    console.log(`Connected to MongoDB at ${uri}`);
-  } catch (error) {
-    console.error("Error while connecting to MongoDB:", error.message);
-  }
+async function connectDatabase(fastify, options) {
+
+ fastify.register(require('@fastify/mongodb'), { url: uri });
+
+
 }
 
 module.exports = fastifyPlugin(connectDatabase);
--- listener.js
+++ listener.js
@@ -1,25 +1,22 @@
-const connectDatabase = require("./db/connection")
-const Fastify = require('fastify')
-const routes = require("./app")
-
-
+// const connectDatabase = require("./db/connection")
+// // const Fastify = require('fastify')
+// const routes = require("./app")
 
 // /**
 //  * @type {import('fastify').FastifyInstance} Instance of Fastify
 //  */
-const fastify = Fastify({
+
+const fastify = require('fastify')({
   logger: true
 })
-// fastify.register(require("./db/connection"))
-// fastify.register(require("./app"))
-console.log(fastify)
+fastify.register(require("./db/connection"))
+fastify.register(require("./app"))
 
-fastify.register(connectDatabase()).then(()=>{
+
 fastify.listen({ port: 9090 }, (err, address) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
   console.log(`Server is now listening on ${address}`)
-})
 })
