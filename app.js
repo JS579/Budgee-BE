bff5d72583c227b16e3f1251a0b21a353870bcca
John Scholefield <johnscho579@gmail.com>
2025-04-15 16:58:35 +0100

    update routes in index file

--- app.js
+++ app.js
@@ -4,40 +4,28 @@ const path = require("node:path");
 const AutoLoad = require("@fastify/autoload");
 const {uri} = require("./db/connection");
 const mongoose = require("mongoose");
-const categoriesRoutes = require("./routes/categoriesRoutes");
-const root = require("./routes/root");
-const expensesRoutes = require("./routes/ExpensesRoutes");
-const budgetRoutes = require("./routes/budgetRoutes")
-const coloursRoutes = require("./routes/colourRoutes")
-
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
-
-const {uri} = require("./db/connection")
-const mongoose = require('mongoose')
-const usersRoutes = require("./routes/UsersRoutes")
+const { root, categoriesRoutes, expensesRoutes, budgetRoutes, coloursRoutes, userRoutes} = require("./routes/indexRoutes")
 
 // Pass --options via CLI arguments in command to enable these options.
 const options = {};
-const options = {}
 
-console.log("uri>>>", uri)
 
-mongoose.connect(uri, {bufferCommands: false});
+
 
 module.exports = async function (fastify, opts) {
   try {
     await mongoose.connect(uri, {bufferCommands: false});
     fastify.register(root);
-    fastify.register(expensesRoutes);
     fastify.register(categoriesRoutes);
+    fastify.register(expensesRoutes);
     fastify.register(budgetRoutes);
-    fastify.register(coloursRoutes)
+    fastify.register(coloursRoutes);
+    fastify.register(userRoutes);
 
 
     // Do not touch the following lines
  
-  fastify.register(usersRoutes)
+
 
     // This loads all plugins defined in plugins
     // those should be support plugins that are reused
@@ -59,10 +47,10 @@ module.exports = async function (fastify, opts) {
   //   })
 };
 
-module.exports.options = options;
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-}
+// module.exports.options = options;
+//   fastify.register(AutoLoad, {
+//     dir: path.join(__dirname, 'plugins'),
+//     options: Object.assign({}, opts)
+//   })
+
 
