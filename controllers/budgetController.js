3186d7dfc6ebb2f7110e0f7cc7809c7349977410
John Scholefield <johnscho579@gmail.com>
2025-04-15 15:13:12 +0100

    add remaining budget endpoints

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -1,4 +1,5 @@
 const Budgets = require("../models/budgetModel");
+const { findById } = require("../models/categoryModel");
 const {
   fetchExpensesByBudgetIdAndCategoryId,
   fetchExpensesByCategoryId,
@@ -13,9 +14,52 @@ async function getAllBudgets(request, reply) {
   }
 }
 
+async function getBudgetById(request, reply) {
+    try {
+        const id = request.params.id;
+        const budget = await Budgets.findById(id);
+        reply.code(200).send(budget);
+      } catch (error) {
+        reply.code(500).send({error: error.message});
+      }
+    }
+
+async function addNewBudget(request, reply) {
+  try {
+    const budget = new Budgets(request.body);
+    const budgetAdded = await budget.save();
+    return budgetAdded;
+  } catch (error) {
+    reply.status(500).send({error: error.message});
+  }
+}
+
+
+async function updateBudget(request, reply) {
+  try {
+    const budget = await Budgets.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      {new: true}
+    );
+    return budget;
+  } catch (error) {
+    reply.status(500).send({error: error.message});
+  }
+}
+
+async function deleteBudget(request, reply) {
+  try {
+    await Budgets.findByIdAndDelete(request.params.id);
+    reply.status(204);
+  } catch (error) {
+    reply.status(500).send({error: error.message});
+  }
+}
+
 
 
 module.exports = {
-  getAllBudgets,
+  getAllBudgets, getBudgetById, addNewBudget, updateBudget, deleteBudget
 };
 
